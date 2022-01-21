8697dcb1a0397d63680d71dad3b4270f5b0d0052
LegoDev <us@conep.one>
2022-01-21 11:38:42 +0000

    Make rules command ephemeral.
    Makes the output of rules command ephemeral, meaning only the executor will see the output.
    Also fixed indentation issue, removed comment, and removed unneeded Enums reference.

--- modules/moderation/rules.js
+++ modules/moderation/rules.js
@@ -1,5 +1,4 @@
 const ChatAction = require("../../src/util/ChatAction");
-const Enums = require("../../src/util/Enums");
 
 module.exports = class extends ChatAction {
 	constructor(Module) {
@@ -10,16 +9,18 @@ module.exports = class extends ChatAction {
 	}
 
 	async execute(interaction) {
-		//i am so sorry leah ;-; - bitl
-		await interaction.reply({ content: 
-		"moderation/rules/one".getLang() +
-		"moderation/rules/two".getLang() + 
-		"moderation/rules/three".getLang() + 
-		"moderation/rules/four".getLang() +
-		"moderation/rules/five".getLang() +
-		"moderation/rules/six".getLang() +
-		"moderation/rules/seven".getLang() +
-		"moderation/rules/eight".getLang() +
-		"moderation/rules/otherinfo".getLang()});
+		await interaction.reply({ 
+			content: 
+				"moderation/rules/one".getLang() +
+				"moderation/rules/two".getLang() + 
+				"moderation/rules/three".getLang() + 
+				"moderation/rules/four".getLang() +
+				"moderation/rules/five".getLang() +
+				"moderation/rules/six".getLang() +
+				"moderation/rules/seven".getLang() +
+				"moderation/rules/eight".getLang() +
+				"moderation/rules/otherinfo".getLang(),
+			ephemeral: true
+		});
 	}
 }
