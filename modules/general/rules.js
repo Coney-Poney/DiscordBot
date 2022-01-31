0f70003c100cc5e581666d1139b8ec44b64ba34b
Twilight/Leah <us@conep.one>
2022-01-31 09:38:39 +0000

    Correct lang references

--- modules/general/rules.js
+++ modules/general/rules.js
@@ -5,21 +5,21 @@ module.exports = class extends ChatAction {
 		super(Module);
 
 		this.name = "rules";
-		this.description = "Recites the rules.";
+		this.description = "Recites the rules";
 	}
 
 	async execute(interaction) {
 		await interaction.reply({ 
 			content: 
-				"moderation/rules/one".getLang() +
-				"moderation/rules/two".getLang() + 
-				"moderation/rules/three".getLang() + 
-				"moderation/rules/four".getLang() +
-				"moderation/rules/five".getLang() +
-				"moderation/rules/six".getLang() +
-				"moderation/rules/seven".getLang() +
-				"moderation/rules/eight".getLang() +
-				"moderation/rules/otherinfo".getLang(),
+				"general/rules/one".getLang() +
+				"general/rules/two".getLang() + 
+				"general/rules/three".getLang() + 
+				"general/rules/four".getLang() +
+				"general/rules/five".getLang() +
+				"general/rules/six".getLang() +
+				"general/rules/seven".getLang() +
+				"general/rules/eight".getLang() +
+				"general/rules/otherinfo".getLang(),
 			ephemeral: true
 		});
 	}
